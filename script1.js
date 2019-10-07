d86683c76ae2c2a772639c63ad60117cbe183935
Catherinexing <54324497+Catherinexing@users.noreply.github.com>
2019-10-07 17:36:58 -0400

    g

--- script1.js
+++ script1.js
@@ -5,6 +5,23 @@
 //   $('#step-0').waypoint(function(direction) {
 //     console.log('Scrolled to waypoint!')
 //     $('#step-0').addClass('animated fadeIn');
+//
+// }, {
+//     offset: '80%'
+// });
+//
+//
+// });
+
+
+// $(document).ready(function(){
+//
+//
+//
+//   $('#scrolly').waypoint(function(direction) {
+//     console.log('Scrolled to waypoint!')
+//     $('#step-0').addClass('.isactive1');
+//
 // }, {
 //     offset: '80%'
 // });
