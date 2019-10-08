b02e94624c98ad9759f27a3c07bf1b0e21ca9e53
Catherinexing <54324497+Catherinexing@users.noreply.github.com>
2019-10-08 16:36:56 -0400

    s

--- script.js
+++ script.js
@@ -26,8 +26,8 @@
         // var stepH = Math.floor(figureHeight * 0.9);
         // step.style('height', stepH + 'px');
 
-        // var stepMT = Math.floor(figureHeight * 0.9);
-        // step.style('margin-bottom', stepMT + 'px');
+        var stepMT = Math.floor(figureHeight * 0.9);
+        step.style('margin-bottom', stepMT + 'px');
     figure
       .style('height', figureHeight + 'px')
       .style('top', figureMarginTop + 'px');
@@ -126,7 +126,7 @@
     // 3. bind scrollama event handlers (this can be chained like below)
     scroller.setup({
         step: '#scrolly article .step',
-          // offset: 0.6,
+          offset: 0.55,
 
         debug: true,
       })
